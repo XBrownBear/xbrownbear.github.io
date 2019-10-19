7612e943635a3c261a529d7f31e0219c563da641
xbrownbear <1070375764@qq.com>
2019-10-19 19:12:03 +0800

    Site updated: 2019-10-19 19:12:00

--- js/src/FunnyTitle.js
+++ js/src/FunnyTitle.js
@@ -1,17 +1,3 @@
-<!--浏览器搞笑标题-->
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/bug-report-16x16.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/images/bug-report-16x16.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+// build time:Sat Oct 19 2019 19:12:00 GMT+0800 (GMT+08:00)
+var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/images/bug-report-16x16.png");document.title="ヽ(●-`Д´-)ノ你丑你就走！";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/bug-report-16x16.png");document.title="ヾ(Ő∀Ő3)ノ你帅就回来！"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
+//rebuild by neat 
