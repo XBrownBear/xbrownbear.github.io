91926642986923291314a412cb325e9cf7f148be
xbrownbear <1070375764@qq.com>
2019-10-19 19:25:32 +0800

    Site updated: 2019-10-19 19:25:30

--- js/src/DigitalRain.js
+++ js/src/DigitalRain.js
@@ -1,3 +1,58 @@
-// build time:Sat Oct 19 2019 19:12:00 GMT+0800 (GMT+08:00)
-window.onload=function(){var a=document.getElementById("DigitalRain");var r=a.getContext("2d");var t=window.screen;var o=a.width=t.width;var n=a.height;a.width=o;a.height=n;var h=12;var e=Math.floor(o/h);var i=[];for(var l=0;l<e;l++){i.push(0)}var v="101101011001010101100101010101010110101011";function f(){r.fillStyle="rgba(0,0,0,.08)";r.fillRect(0,0,o,n);r.font="600 "+h+"px  Georgia";r.fillStyle=["#008000"];for(var t=0;t<e;t++){var l=Math.floor(Math.random()*v.length);var f=t*h;var d=i[t]*h;r.fillText(v[l],f,d);if(d>=a.height&&Math.random()>.99){i[t]=0}i[t]++}}function d(){var a=Math.floor(Math.random()*256);var r=Math.floor(Math.random()*256);var t=Math.floor(Math.random()*256);return"rgb("+a+","+r+","+t+")"}f();setInterval(f,35)};
-//rebuild by neat 
+window.onload = function(){
+    //获取画布对象。canvas样式在next\source\css\_schemes\Gemini下
+    var canvas = document.getElementById("DigitalRain");
+    //获取画布的上下文
+    var context =canvas.getContext("2d");
+    var s = window.screen;
+    var W = canvas.width = s.width;
+    var H = canvas.height;
+    //获取浏览器屏幕的宽度和高度
+    //var W = window.innerWidth;
+    //var H = window.innerHeight;
+    //设置canvas的宽度和高度
+    canvas.width = W;
+    canvas.height = H;
+    //每个文字的字体大小
+    var fontSize = 12;
+    //计算列
+    var colunms = Math.floor(W /fontSize);	
+    //记录每列文字的y轴坐标
+    var drops = [];
+    //给每一个文字初始化一个起始点的位置
+    for(var i=0;i<colunms;i++){
+        drops.push(0);
+    }
+    //运动的文字
+    var str ="101101011001010101100101010101010110101011";
+    //4:fillText(str,x,y);原理就是去更改y的坐标位置
+    //绘画的函数
+    function draw(){
+        context.fillStyle = "rgba(0,0,0,.08)";//遮盖层，背景色，原来是rgba(238,238,238,.08)
+        context.fillRect(0,0,W,H);
+        //给字体设置样式
+        context.font = "600 "+fontSize+"px  Georgia";
+        //给字体添加颜色
+        //context.fillStyle = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"][parseInt(Math.random() * 10)];//randColor();可以rgb,hsl, 标准色，十六进制颜色
+        context.fillStyle = ["#008000"];//将上面随机颜色修改成绿色
+        //写入画布中
+        for(var i=0;i<colunms;i++){
+            var index = Math.floor(Math.random() * str.length);
+            var x = i*fontSize;
+            var y = drops[i] *fontSize;
+            context.fillText(str[index],x,y);
+            //如果要改变时间，肯定就是改变每次他的起点
+            if(y >= canvas.height && Math.random() > 0.99){
+                drops[i] = 0;
+            }
+            drops[i]++;
+        }
+    };
+    function randColor(){//随机颜色
+        var r = Math.floor(Math.random() * 256);
+        var g = Math.floor(Math.random() * 256);
+        var b = Math.floor(Math.random() * 256);
+        return "rgb("+r+","+g+","+b+")";
+    }
+    draw();
+    setInterval(draw,35);
+};
--- js/src/click_show_text.js
+++ js/src/click_show_text.js
@@ -1,3 +1,31 @@
-// build time:Sat Oct 19 2019 19:12:00 GMT+0800 (GMT+08:00)
-var f_idx=0;var c_idx=0;jQuery(document).ready(function(e){e("body").click(function(f){var a=new Array("阳光","从容","低调","帅气","睿智","机智","乐观","谦逊","潇洒","善良","温柔","暖心","文明","正直","自由","敬业","诚信","友善");var d=new Array("#ff0000","#eb4310","#f6941d","#fbb417","#ffff00","#cdd541","#99cc33","#3f9337","#219167","#239676","#24998d","#1f9baa","#0080ff","#3366cc","#333399","#003366","#800080","#a1488e","#c71585","#bd2158");var c=e("<span />").text(a[f_idx]);f_idx=(f_idx+1)%a.length;c_idx=(c_idx+1)%d.length;var i=f.pageX,n=f.pageY;c.css({"z-index":1e74,top:n-20,left:i,position:"absolute","font-weight":"bold",color:d[c_idx]});e("body").append(c);c.animate({top:n-180,opacity:0},3e3,function(){c.remove()})})});
-//rebuild by neat 
+/* 鼠标点击文字特效 */
+    var f_idx = 0;
+    var c_idx = 0;
+    jQuery(document).ready(function($) {
+        $("body").click(function(e) {
+            var font = new Array("阳光", "从容", "低调", "帅气", "睿智", "机智", "乐观", "谦逊", "潇洒", "善良", "温柔", "暖心", "文明", "正直", "自由", "敬业", "诚信", "友善");
+            var color = new Array('#ff0000','#eb4310','#f6941d','#fbb417','#ffff00','#cdd541','#99cc33','#3f9337','#219167','#239676','#24998d','#1f9baa','#0080ff','#3366cc','#333399','#003366','#800080','#a1488e','#c71585','#bd2158');
+            var $i = $("<span />").text(font[f_idx]);
+            f_idx = (f_idx + 1) % font.length;
+            c_idx = (c_idx + 1) % color.length;
+            var x = e.pageX,
+                y = e.pageY;
+            $i.css({
+                "z-index": 99999999999999999999999999999999999999999999999999999999999999999999999999 ,
+                "top": y - 20,
+                "left": x,
+                "position": "absolute",
+                "font-weight": "bold",
+                "color": color[c_idx]
+            });
+            $("body").append($i);
+            $i.animate({
+                    "top": y - 180,
+                    "opacity": 0
+                },
+                3000,
+                function() {
+                    $i.remove();
+                });
+        });
+    });
